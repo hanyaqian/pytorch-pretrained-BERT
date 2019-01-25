0e695385e72ae6e5bc3587b7f67702a27ac2c3e5
Paul Michel <pmichel31415@gmail.com>
2019-01-25 10:50:54 -0500

    Adjust experiment scripts

--- experiments/heads_pruning_MRPC.sh
+++ experiments/heads_pruning_MRPC.sh
@@ -3,7 +3,7 @@
 OPTIONS=${1:-""}
 
 here="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source $here/prepare_task.sh MNLI
+source $here/prepare_task.sh MRPC
 
 echo $base_acc
 echo $part
@@ -12,11 +12,6 @@ do
     echo -n "$percent	"
     prune_options="--do_prune --eval_pruned --prune_percent $percent $OPTIONS"
     acc=$(run_eval "$prune_options" | grep eval_accuracy | rev | cut -d" " -f1 | rev)
-    for head in `seq 1 12`
-    do
-        mask_str="${layer}:${head}"
-        printf " & %.5f" $(echo "$acc - $base_acc" | bc )
-    done
-    echo " \\\\"
+    echo "$acc"
 done
 
